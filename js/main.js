f807046f1e0a573364d257b4328e44a102522d23
juantreses <jandries@live.be>
2020-03-20 10:56:07 +0100

    Deleting js folder because it is unnecessary

--- js/main.js
+++
@@ -1,9 +0,0 @@
-const listRef = document.getElementById("tasks");
-
-listRef.addEventListener("click", function (e) {
-    e.preventDefault();
-    if (e.target.nodeName === "BUTTON") {
-        const idToEdit = e.target.parentElement.parentElement.dataset.mijnid;
-        console.log(idToEdit)
-    }
-});
